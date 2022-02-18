925be00df255510d46cf7d37f8f60c9d649443e0
Shumaev <shumaevdmit@gmail.com>
2022-02-18 18:03:01 +0300

    working server with hello-message endpoint

---
+++ conntrollers/controller.js
@@ -0,0 +1,11 @@
+class Controller {
+
+    sendHelloMessage(req, res) {
+        res.json({
+            message: 'Hello my dear friend!'
+        })
+    }
+
+}
+
+module.exports = new Controller()
