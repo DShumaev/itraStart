0646578ec7f5ff5e60211be7ffbd77aa9db0d343
Shumaev <shumaevdmit@gmail.com>
2022-03-01 20:15:27 +0300

    fixed something mistake on pullrequest review

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require('express')
-const mainRouter = require('./routers/mainRouter')
+const express = require("express");
+const mainRouter = require("./routers/mainRouter");
 
-const PORT = process.env.PORT || 5000
-const app = express()
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-app.use(express.json())
-app.use('/', mainRouter)
-
+app.use(express.json());
+app.use("/", mainRouter);
 
 function startServer() {
-    try {
-        app.listen(PORT, () => {
-            console.log(`server started on port ${PORT}`)
-        })
-    } catch (e) {
-        console.log('Problem with starting HTTP server')
-        process.exit(1)
-    }
+  try {
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log("Problem with starting HTTP server");
+    process.exit(1);
+  }
 }
 
-
-startServer()
+startServer();
