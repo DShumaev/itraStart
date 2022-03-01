d81d9d000039c23d1a156cf81218fccd4fb70a61
Shumaev <shumaevdmit@gmail.com>
2022-03-01 23:02:40 +0300

    added error handler in index.js

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const mainRouter = require("./routers/mainRouter");
+const errorHandler = require("./middleware/index");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use("/", mainRouter);
-
-function startServer() {
-  try {
-    app.listen(PORT, () => {
-      console.log(`server started on port ${PORT}`);
-    });
-  } catch (e) {
-    console.log("Problem with starting HTTP server");
-    process.exit(1);
-  }
-}
-
-startServer();
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
