35692427c7f491b889ad52829e1d29e6b88fdaea
Shumaev <shumaevdmit@gmail.com>
2022-03-01 12:54:15 +0300

    added unit test for user service and fixed 2 bags in service

--- services/dbUserServices.js
+++ services/dbUserServices.js
@@ -61,7 +61,7 @@ class UserServices {
         return false;
       }
       return userData.dataValues;
-    } catch {
+    } catch (e) {
       return false;
     }
   }
@@ -82,9 +82,10 @@ class UserServices {
         return (counter += 1);
       }, 0);
 
-      if (!(countUserFields > 0)) {
+      if (!(countUserFields === this.userDbField.length)) {
         return false;
       }
+
       const updatedData = await this.userModel.update(newUserData, {
         where: { id },
       });
@@ -98,13 +99,17 @@ class UserServices {
   }
 
   async deleteUser(id) {
-    const isUserDelete = await this.userModel.destroy({
-      where: { id },
-    });
+    try {
+      const isUserDelete = await this.userModel.destroy({
+        where: { id },
+      });
 
-    if (isUserDelete) {
-      return true;
-    } else {
+      if (isUserDelete) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch {
       return false;
     }
   }
