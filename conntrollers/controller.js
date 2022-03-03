6a17c4b02577bbdb4cfcdc1b519af37e33fe63c4
Dima <shumaevdmit@yandex.ru>
2022-03-03 17:25:21 +0300

    fix trouble with controller folder

--- conntrollers/controller.js
+++
@@ -1,9 +0,0 @@
-class Controller {
-  sendHelloMessage(req, res) {
-    res.json({
-      message: "Hello my dear friend!",
-    });
-  }
-}
-
-module.exports = new Controller();
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Controller = require("../conntrollers/controller");
+const Controller = require("../controllers/controller");
 
 router.get("/hello", Controller.sendHelloMessage);
 
